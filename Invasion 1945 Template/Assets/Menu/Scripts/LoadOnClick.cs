bf1922659ee9c0e27fea111a2dd482035c42584d
metra34 <artem.clement@mail.utoronto.ca>
2016-02-16 12:54:15 -0500

    Adding Menu Stuff
    - added various main menu screens
    - added game over screens
    - buttons work when scenes added to buildSettings

---
+++ Invasion 1945 Template/Assets/Menu/Scripts/LoadOnClick.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadOnClick : MonoBehaviour {
+
+	//public GameObject loadingImage;
+
+	public void LoadScene(string level)
+	{
+		//loadingImage.SetActive(true);
+		SceneManager.LoadScene(level);
+	}
+
+	public void QuitGame (){
+		Application.Quit ();
+	}
+}
