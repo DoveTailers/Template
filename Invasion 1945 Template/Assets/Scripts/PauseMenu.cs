fc4e22fe5d8c2b6ede0d1dfe1f2165306b288e06
metra34 <artem.clement@mail.utoronto.ca>
2016-02-23 10:02:24 -0500

    Added working pause menu to puzzle_1
    -- important changes to TagManager.asset
    -- important changes to GraphicSettings.asset

---
+++ Invasion 1945 Template/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour {
+
+	public bool isPaused;
+
+	public GameObject pauseMenuCanvas;
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (isPaused) {
+			pauseMenuCanvas.SetActive (true);
+		} else {
+			pauseMenuCanvas.SetActive (false);
+		}
+
+		if (Input.GetKeyUp (KeyCode.Escape)) 
+		{
+			isPaused = !isPaused;
+		}
+	}
+
+	public void Continue ()
+	{
+		isPaused = false;
+	}
+
+	public void Restart ()
+	{
+		Application.LoadLevel (Application.loadedLevelName);
+	}
+
+	public void Quit ()
+	{
+		Application.LoadLevel ("mainmenub");
+	}
+}
