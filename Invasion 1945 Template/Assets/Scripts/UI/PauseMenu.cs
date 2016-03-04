b29a8e1ed9b9183ebf45553e82d41665a14e67ac
metra34 <artem.clement@mail.utoronto.ca>
2016-03-04 18:47:12 -0500

    adsafasffa

---
+++ Invasion 1945 Template/Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour {
+
+	public bool isPaused;
+
+	public GameObject pauseMenuCanvas;
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (isPaused) {
+			Time.timeScale = 0f;
+			pauseMenuCanvas.SetActive (true);
+		} else {
+			pauseMenuCanvas.SetActive (false);
+			Time.timeScale = 1f;
+		}
+
+		if (Input.GetKeyUp (KeyCode.Escape)) 
+		{
+			isPaused = !isPaused;
+		}
+	}
+
+	public void Continue ()
+	{
+		isPaused = false;
+	}
+
+	public void Restart ()
+	{
+		Application.LoadLevel (Application.loadedLevelName);
+	}
+
+	public void Quit ()
+	{
+		Application.LoadLevel ("mainmenub");
+	}
+}
