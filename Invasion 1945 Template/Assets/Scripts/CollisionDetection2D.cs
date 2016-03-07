3a258e69e954c3bdc0b06e396991e0003dc6f115
JacobSteele <jacob.steele@mail.utoronto.ca>
2016-03-07 18:56:22 -0500

    Manually Merge new-Waves2

---
+++ Invasion 1945 Template/Assets/Scripts/CollisionDetection2D.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionDetection2D : MonoBehaviour {
+
+	public static bool reverse = false;
+	public static bool collision = false;
+	public GameObject Explosion;
+
+
+	// Use this for initialization
+	void Start () {
+		reverse = false;
+		collision = false;
+	}
+
+
+	void OnTriggerEnter2D(Collider2D collider) {
+		if (collider.gameObject.CompareTag ("Coin")) {
+			Destroy (collider.gameObject);
+			reverse = true;
+		}
+
+	}
+
+	void OnCollisionEnter2D(Collision2D collider) {
+		if (!(collider.gameObject.CompareTag ("Maze")) ) {
+			Instantiate(Explosion, this.gameObject.transform.position, Quaternion.identity);
+			Destroy (this.gameObject);
+			collision = true;
+		}
+	}
+
+	void resetValues() {
+
+	}
+}
--- Invasion 1945 Template/Assets/Scripts/DestoryByContact.cs
+++ Invasion 1945 Template/Assets/Scripts/DestoryByContact.cs
@@ -4,13 +4,18 @@ using System.Collections;
 public class DestoryByContact : MonoBehaviour {
 
     public GameObject explosion;
+	//public GameObject explosionOther;
     void OnTriggerEnter2D(Collider2D other) 
 	{
-		if (other.tag == "Boundary")
-		{
+		if (other.tag == "Boundary") {
+			return;
+		} else if (other.tag == "BlackHole") {
+			return;
+		} else if (other.tag == "Shootable") {
 			return;
 		}
         Instantiate(explosion, transform.position, transform.rotation);
+		//Instantiate(explosionOther, other.transform.position, other.transform.rotation);
         Destroy(other.gameObject);
         Destroy(gameObject);
 
